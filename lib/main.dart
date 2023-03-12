b4c6659af63867a20e8e445929f20d2f83af08ca
stepan103ua <stepan8877ua@gmail.com>
2023-03-12 19:41:25 +0200

    Add bloc, flutter_bloc, equatable packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app/view/app.dart';
+
 void main() {
   runApp(const FoodDeliveryApp());
-}
-
-class FoodDeliveryApp extends StatelessWidget {
-  const FoodDeliveryApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => const MaterialApp();
 }

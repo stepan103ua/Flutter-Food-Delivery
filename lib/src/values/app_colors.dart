b24ff06079c8c5f4d58ed4b636434869a9b01d57
Xaiodo <rostislavgovbak@gmail.com>
2023-04-05 12:52:49 +0300

    Updated app colors

--- lib/src/values/app_colors.dart
+++ lib/src/values/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color green = Color(0xff2A9D8F);
   static const Color white = Colors.white;
+  static const Color lightGrey = Color.fromARGB(255, 209, 208, 208);
   static const Color greenBackground = Color(0xff264653);
   static const Color error = Colors.redAccent;
   static const Color hint = Color(0xff594C74);

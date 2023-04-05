078260ffe7ace07c6f387cfb2ebfed522d9336a4
Xaiodo <rostislavgovbak@gmail.com>
2023-04-05 12:54:55 +0300

    Implemented login cubit logic

--- lib/src/pages/auth/pages/login/login_cubit/login_cubit.dart
+++ lib/src/pages/auth/pages/login/login_cubit/login_cubit.dart
@@ -1,8 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_delivery/src/models/validation_model.dart';
+import 'package:food_delivery/src/values/app_validation.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit() : super(const LoginInitial());
+
+  void onEmailUpdate(String email) {
+    emit(state.updatedEmail(email: email));
+  }
+
+  void onPasswordUpdate(String password) {
+    emit(state.updatedPassword(password: password));
+  }
+
+  void onLoginPressed() {}
 }

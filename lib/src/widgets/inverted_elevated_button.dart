622788aafcf1a1693216608600aae7660613f155
Xaiodo <rostislavgovbak@gmail.com>
2023-04-05 12:55:38 +0300

    Created inverted elevated button

---
+++ lib/src/widgets/inverted_elevated_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/src/values/theme.dart';
+
+class InvertedElevatedButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+  const InvertedElevatedButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) => Theme(
+        data: Theme.of(context)
+            .copyWith(elevatedButtonTheme: invertedElevatedButton),
+        child: ElevatedButton(onPressed: onPressed, child: child),
+      );
+}

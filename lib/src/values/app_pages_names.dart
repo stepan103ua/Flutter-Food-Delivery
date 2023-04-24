dced8b229d1a078275dd17dad781be26b0fc9bdd
stepan103ua <stepan8877ua@gmail.com>
2023-04-24 12:56:58 +0300

    Add pages names

--- lib/src/values/app_pages_names.dart
+++ lib/src/values/app_pages_names.dart
@@ -3,4 +3,7 @@ abstract class AppPagesNames {
   static const login = '/login';
   static const register = '/register';
   static const splashScreen = '/splashScreen';
+
+  static const authorized = '/authorized';
+  static const bottomNavigation = '/authorized/bottom_navigation';
 }

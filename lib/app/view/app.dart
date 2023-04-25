75dae220d5b537a9e6b9ae7b2522aa98f2478fd2
stepan103ua <stepan8877ua@gmail.com>
2023-04-25 11:21:41 +0300

    Remove firebase usages in the code

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -2,17 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_delivery/app/app_bloc/bloc/app_bloc.dart';
 import 'package:food_delivery/app/view/app_view.dart';
-import 'package:food_delivery/src/providers/dependencies_provider.dart';
 
 class FoodDeliveryApp extends StatelessWidget {
   const FoodDeliveryApp({super.key});
 
   @override
-  Widget build(BuildContext context) => DependenciesProvider(
-        child: BlocProvider(
-          create: (context) =>
-              AppBloc(firebaseAuth: RepositoryProvider.of(context)),
-          child: const FoodDeliveryAppView(),
-        ),
-      );
+  Widget build(BuildContext context) => BlocProvider(
+    create: (context) => AppBloc(),
+    child: const FoodDeliveryAppView(),
+  );
 }

1cc6ed7ac24baac69ad092e20e132a5757892a85
stepan103ua <stepan8877ua@gmail.com>
2023-04-26 15:51:45 +0300

    Add auth repository to dependencies provider

--- lib/src/providers/dependencies_provider.dart
+++ lib/src/providers/dependencies_provider.dart
@@ -1,5 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:food_delivery/src/dao/auth_dao.dart';
+import 'package:food_delivery/src/repositories/auth_repository.dart';
+import 'package:food_delivery/src/services/auth_service.dart';
 
 class DependenciesProvider extends StatelessWidget {
   const DependenciesProvider({super.key, required this.child});
@@ -8,7 +13,30 @@ class DependenciesProvider extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => MultiRepositoryProvider(
-        providers: [],
+        providers: [
+          RepositoryProvider(
+            create: (context) =>
+                BaseOptions(baseUrl: 'http://localhost:8000/api'),
+          ),
+          RepositoryProvider(
+            create: (context) => Dio(RepositoryProvider.of(context)),
+          ),
+          RepositoryProvider(create: (context) => const FlutterSecureStorage()),
+          RepositoryProvider(
+            create: (context) =>
+                AuthService(dio: RepositoryProvider.of(context)),
+          ),
+          RepositoryProvider(
+            create: (context) =>
+                AuthDao(storage: RepositoryProvider.of(context)),
+          ),
+          RepositoryProvider(
+            create: (context) => AuthRepository(
+              service: RepositoryProvider.of(context),
+              dao: RepositoryProvider.of(context),
+            ),
+          ),
+        ],
         child: child,
       );
 }

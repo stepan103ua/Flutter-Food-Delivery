eae352ecaef8303181c50a78aa626ae0fdb0990b
stepan103ua <stepan8877ua@gmail.com>
2023-05-07 17:22:56 +0300

    Refactor cart navigation

--- lib/src/pages/authorized/pages/bottom_navigation/pages/cart/cart_cubit/cart_cubit.dart
+++ lib/src/pages/authorized/pages/bottom_navigation/pages/cart/cart_cubit/cart_cubit.dart
@@ -1,8 +1,10 @@
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:food_delivery/src/navigation/navigation.dart';
+import 'package:food_delivery/src/navigation/navigation_cubit.dart';
+import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/cart/pages/cart_items/view/cart_items_page.dart';
 
 part 'cart_state.dart';
 
-class CartCubit extends Cubit<CartState> {
-  CartCubit() : super(CartInitial());
+class CartCubit extends NavigationCubit<CartState> {
+  CartCubit() : super(CartState(pages: [CartItemsPage.page()]));
 }
--- lib/src/pages/authorized/pages/bottom_navigation/pages/cart/view/cart_view.dart
+++ lib/src/pages/authorized/pages/bottom_navigation/pages/cart/view/cart_view.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:food_delivery/src/navigation/navigation_router.dart';
+import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/cart/cart_cubit/cart_cubit.dart';
 
 class CartView extends StatelessWidget {
   const CartView({super.key});
 
   @override
-  Widget build(BuildContext context) => const Scaffold(
-        body: Center(
-          child: Text('Cart'),
-        ),
-      );
+  Widget build(BuildContext context) =>
+      NavigationRouter(navigationCubit: context.read<CartCubit>());
 }

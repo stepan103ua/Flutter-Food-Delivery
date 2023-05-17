3a0113a8c32255680acc238cc7b873ae766deb0f
Xaiodo <rostislavgovbak@gmail.com>
2023-05-17 21:49:11 +0300

    Implemented settings callback

--- lib/app/app_bloc/bloc/app_bloc.dart
+++ lib/app/app_bloc/bloc/app_bloc.dart
@@ -1,8 +1,7 @@
-import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:food_delivery/src/pages/auth/auth_cubit/auth_callback.dart';
+import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/settings/settings_cubit/settings_callback.dart';
 import 'package:food_delivery/src/pages/splash_screen/models/initialization_result.dart';
 import 'package:food_delivery/src/pages/splash_screen/splash_screen_cubit/splash_screen_callback.dart';
 
@@ -11,14 +10,10 @@ part 'app_event.dart';
 part 'app_state.dart';
 
 class AppBloc extends Bloc<AppEvent, AppState>
-    implements AuthCallback, SplashScreenCallback {
-  late final StreamSubscription _authSubscription;
-
+    implements AuthCallback, SplashScreenCallback, SettingsCallback {
   AppBloc() : super(AppInitializing()) {
     on<AppGoToAuthenticationEvent>(_onGoToAuthenticationEvent);
     on<AppGoToAuthenticatedEvent>(_onGoToAuthenticatedEvent);
-
-    //_init();
   }
 
   void _onGoToAuthenticationEvent(
@@ -41,9 +36,8 @@ class AppBloc extends Bloc<AppEvent, AppState>
   }
 
   @override
-  Future<void> close() async {
-    await _authSubscription.cancel();
-    return super.close();
+  void onLogout() {
+    add(AppGoToAuthenticationEvent());
   }
 
   @override

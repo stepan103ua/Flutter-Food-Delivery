ce4a9f801c9ff9c5a1dce5dfd114cd24432fed64
Xaiodo <rostislavgovbak@gmail.com>
2023-05-17 12:37:51 +0300

    Created product detail callback

--- lib/src/pages/authorized/pages/bottom_navigation/pages/home/home_cubit/home_cubit.dart
+++ lib/src/pages/authorized/pages/bottom_navigation/pages/home/home_cubit/home_cubit.dart
@@ -1,17 +1,19 @@
 import 'package:equatable/equatable.dart';
 import 'package:food_delivery/src/navigation/navigation_cubit.dart';
 import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/home/pages/category/category_cubit/category_callback.dart';
+import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/home/pages/product_detail/product_detail_cubit/product_detail_callback.dart';
 import 'package:food_delivery/src/pages/authorized/pages/bottom_navigation/pages/home/pages/products/view/products_page.dart';
 
 import '../../../../../../../models/category.dart';
 import '../../../../../../../navigation/navigation.dart';
 import '../pages/category/view/category_page.dart';
+import '../pages/product_detail/view/product_detail_page.dart';
 import '../pages/products/products_cubit/products_callback.dart';
 
 part 'home_state.dart';
 
 class HomeCubit extends NavigationCubit<HomeState>
-    implements ProductsCallback, CategoryCallback {
+    implements ProductsCallback, CategoryCallback, ProductDetailCallback {
   HomeCubit() : super(HomeState(pages: [CategoryPage.page()]));
 
   @override
@@ -22,5 +24,10 @@ class HomeCubit extends NavigationCubit<HomeState>
   @override
   void onCategoryBack() {
     emit(state.pop());
+  }
+
+  @override
+  void onProductDetailOpen(String productName) {
+    emit(state.pushPage(ProductDetailPage.page(productName)));
   }
 }
---
+++ lib/src/pages/authorized/pages/bottom_navigation/pages/home/pages/product_detail/product_detail_cubit/product_detail_callback.dart
@@ -0,0 +1,3 @@
+abstract class ProductDetailCallback {
+  onProductDetailOpen(String productName);
+}
